4696360758913ba313f69309dee266185a8d32c2
MinTearMT <69701762+MinTearMT@users.noreply.github.com>
2020-08-15 10:07:41 +0900

    implement MinT

--- discordbot.py
+++ discordbot.py
@@ -39,4 +39,23 @@ client.on('ready', message =>
 });
 
 
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('おはよう'):
+        await message.channel.send('おはよ！　つД｀)')
+
+client.run('your token here')
+
+
 bot.run(token)
