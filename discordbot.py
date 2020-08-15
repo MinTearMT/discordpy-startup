2d7f105121b8624051e33df72b977cb0abaa50a0
MinTearMT <69701762+MinTearMT@users.noreply.github.com>
2020-08-15 10:02:42 +0900

    implement MinT

--- discordbot.py
+++ discordbot.py
@@ -23,4 +23,20 @@ async def あ(ctx):
     await ctx.send('あ！')
 
 
+// Response for Uptime Robot
+const http = require('http');
+http.createServer(function(request, response)
+{
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Discord bot is active now \n');
+}).listen(3000);
+client.on('ready', message =>
+{
+  client.user.setPresence({ game: { name: 'MinT.net' } });  
+  console.log('bot is ready!');
+});
+
+
+
+
 bot.run(token)
