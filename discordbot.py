5b2ee0ef70f5eded6f1578bbdbd1c584113d876e
MinTearMT <69701762+MinTearMT@users.noreply.github.com>
2020-08-15 10:18:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,23 +23,4 @@ async def あ(ctx):
     await ctx.send('あ！')
 
 
-import discord
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('おはよう'):
-        await message.channel.send('おはよ！　つД｀)')
-
-client.run('your token here')
-
-
 bot.run(token)
